aaf76fa976e44ed5b34499e8b4e3a0a1d65ce99a
张永波 <327255639@qq.com>
2019-02-27 09:18:23 +0800

    maven_ssm

---
+++ maven_ssm/maven_ssm_utils/src/main/java/com/itcast/utils/JdbcUtil.java
@@ -0,0 +1,77 @@
+package com.itcast.utils;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JdbcUtil {
+    /*
+    * JDBC的工具类,使用的是druid
+    * */
+    private static DataSource ds = null;
+
+    static {
+        try {
+            Properties pro = new Properties();
+            pro.load(JdbcUtil.class.getClassLoader().getResourceAsStream("druid.properties"));
+            ds = DruidDataSourceFactory.createDataSource(pro);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+    public static DataSource getDataSource(){
+        return ds;
+    }
+
+    public static void close(Statement stmt, Connection conn) {
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void close(ResultSet rs, Statement stmt, Connection conn) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
