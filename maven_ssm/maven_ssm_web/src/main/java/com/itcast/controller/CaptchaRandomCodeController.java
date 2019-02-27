028c990734705121f0fb0de36903e7333a29330f
张永波 <327255639@qq.com>
2019-02-27 16:39:05 +0800

    maven_ssm

--- maven_ssm/maven_ssm_web/src/main/java/com/itcast/controller/CaptchaRandomCodeController.java
+++ maven_ssm/maven_ssm_web/src/main/java/com/itcast/controller/CaptchaRandomCodeController.java
@@ -6,6 +6,7 @@ import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,54 +32,30 @@ public class CaptchaRandomCodeController {
     }
 
     @RequestMapping(value = "/image")
-    public ModelAndView captchaHandler(HttpServletRequest request, HttpServletResponse response)throws Exception{
-//        // 禁止server端缓存
-//        response.setDateHeader("Expires", 0);
-//        // 设置标准的 HTTP/1.1 no-cache headers.
-//        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
-//        // 设置IE扩展 HTTP/1.1 no-cache headers (use addHeader).
-//        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
-//        // 设置标准 HTTP/1.0 不缓存图片
-//        response.setHeader("Pragma", "no-cache");
-//        //设置返回数据MIME类型
-//        response.setContentType("image/jpeg");
-//        //为图片创建文本副本
-//        String capText = captchaProducer.createText();
-//        //将文本放入session域中
-//        request.getSession().setAttribute(Constants.KAPTCHA_SESSION_CONFIG_KEY, capText);
-//        System.out.println("Session 验证码是：" + request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY));
-//        //创建带文本的图片，验证码图片
-//        BufferedImage image = captchaProducer.createImage(capText);
-//        //获取响应流
-//        ServletOutputStream os = response.getOutputStream();
-//        //输出图片
-//        ImageIO.write(image, "jpg", os);
-//        //释放资源
-//        os.flush();
-//        os.close();
-        response.setDateHeader("Expires", 0);// 禁止server端缓存
+    public void captchaHandler(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 禁止server端缓存
+        response.setDateHeader("Expires", 0);
         // 设置标准的 HTTP/1.1 no-cache headers.
         response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
         // 设置IE扩展 HTTP/1.1 no-cache headers (use addHeader).
         response.addHeader("Cache-Control", "post-check=0, pre-check=0");
-        response.setHeader("Pragma", "no-cache");// 设置标准 HTTP/1.0 不缓存图片
-        response.setContentType("image/jpeg");// 返回一个 jpeg 图片，默认是text/html(输出文档的MIMI类型)
-        String capText = captchaProducer.createText();// 为图片创建文本
-
-        // 将文本保存在session中。这里就使用包中的静态变量吧
-        request.getSession().setAttribute(Constants.KAPTCHA_SESSION_KEY, capText);
-
-        BufferedImage bi = captchaProducer.createImage(capText); // 创建带有文本的图片
-        ServletOutputStream out = response.getOutputStream();
-        // 图片数据输出
-        ImageIO.write(bi, "jpg", out);
-        try {
-            out.flush();
-        } finally {
-            out.close();
-        }
-
+        // 设置标准 HTTP/1.0 不缓存图片
+        response.setHeader("Pragma", "no-cache");
+        //设置返回数据MIME类型
+        response.setContentType("image/jpeg");
+        //为图片创建文本副本
+        String capText = captchaProducer.createText();
+        //将文本放入session域中
+        request.getSession().setAttribute(Constants.KAPTCHA_SESSION_CONFIG_KEY, capText);
         System.out.println("Session 验证码是：" + request.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY));
-        return null;
+        //创建带文本的图片，验证码图片
+        BufferedImage image = captchaProducer.createImage(capText);
+        //获取响应流
+        ServletOutputStream os = response.getOutputStream();
+        //输出图片
+        ImageIO.write(image, "jpg", os);
+        //释放资源
+        os.flush();
+        os.close();
     }
 }
